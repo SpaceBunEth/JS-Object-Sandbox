19e5e093d44e8ce643a7f1c5f17e6bbe9579207c
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-09-20 17:16:32 +0000

    Put object in object, shorthand function and if in object Gameboy

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,28 @@
+console.log('Object Gameboy')
 const Gameboy = {
     GameboyState: true, //GameBoy ON
-    Levels = {
-        PowerLevel: 100%,
-        VolumeLevel: 100%,
-        ScreenBrightnessLevel: 100%
-    }
+    ScreanState: true,
+    SoundState: false,
+    PowerSource: ['Batteries','Charger'],
 
+    Levels: Levels = {
+        PowerLevel: 100,
+        VolumeLevel: 100,
+        ScreenBrightnessLevel: 100
+    },
+
+    Inputs: Inputs = {
+        VolumeControl: false,
+        DPad: false,
+        Abutton: false,
+        Bbutton: false,
+        PauseButton: false,
+        SelectButton: false,
+        PowerButton: false,
+        GameCartridge: false
+    },
+
+    ComboAB: () => (Inputs.Abutton === false && Inputs.Bbutton === false ? console.log("no Combo") : console.log('Combo'))
+    
 
 }
